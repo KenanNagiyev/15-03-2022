3ecdda40bad9e700dc58fa98111141d873d32c11
KenanNagiyev <788lnmp@code.edu.az>
2022-03-15 11:16:51 +0400

    new file

---
+++ ConsoleApp18/Model/Weapon.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp18.Model
+{
+    class Weapon
+    {
+        public int bulletCap;
+        public int bulletCount;
+        public double time;
+        public bool auto = true;
+
+        public Weapon(int bulletCap, int bulletCount, double time, bool auto)
+        {
+            this.bulletCap = bulletCap;
+            this.bulletCount = bulletCount;
+            this.time = time;
+            this.auto = auto;
+            if (bulletCount > bulletCap)
+            {
+                Console.WriteLine("Xeberdarliq:gulle sayi tutumdan cox ola bilmez");
+            }
+            
+        }
+        public void Shoot()
+        {
+
+
+            if (bulletCount <= 0)
+            {
+                Console.WriteLine("Gulle yoxdur");
+            }
+            else if (auto == true)
+            {
+                Console.WriteLine("azalir");
+                bulletCount--;
+
+                if (bulletCount > 0)
+                {
+                    bulletCount--;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Gulle yoxdur");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("azalir");
+            }
+        }
+        
+        public void Fire()
+        {
+            double shootbullets = 0;
+            if (bulletCount == 0)
+            {
+                Console.WriteLine("gulle  yoxdur");
+                return;
+            }
+            if (auto)
+            {
+
+
+                while (bulletCount > 0)
+                {
+                    bulletCount--;
+                    shootbullets++;
+
+                }
+            }
+            shootbullets *= (bulletCap / time);
+            Console.WriteLine($"gulle bu qeder vaxta atildi:{shootbullets}");
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine(bulletCap-bulletCount);
+        }
+        public void Reload()
+        {
+            if (bulletCap - bulletCount == 0)
+            {
+                Console.WriteLine("doludur");
+            }
+            else
+            {
+                Console.WriteLine(bulletCap - bulletCount);
+                bulletCount = bulletCap;
+
+
+            }
+        }
+    }
+}
+    
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ConsoleApp18.Model;
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon w = new Weapon(10,2, 4, true);
+            w.Shoot();
+            w.Fire();
+            w.GetRemainBulletCount();
+            w.Reload();
+
+            int choise = 0;
+            while (choise!=4)
+            {
+                Console.WriteLine("choise");
+                choise = Convert.ToInt32(Console.ReadLine());
+                switch (choise)
+                {
+                    case 0:
+                        {
+                            w.Shoot();
+                        }
+                        break;
+                    case 1:
+                        {
+                            w.Fire();
+                        }
+                        break;
+                    case 2:
+                        {
+                            w.GetRemainBulletCount();
+                        }
+                        break;
+                    case 3:
+                        {
+                            w.Reload();
+                        }
+                        break;
+                   
+                    default:
+                        {
+
+                        }
+                        break;
+
+
+                }
+            }
+
+        }
+
+    }
+}
